395625457379ed7bc54522b4603c12c22b6cbdd0
darth.cabs <dav.cardoso@gmail.com>
2016-10-10 11:20:21 -0300

    classes cliente-advogado-pessoa (#2)

--- src/br/com/fiap/beans/Cliente.java
+++ src/br/com/fiap/beans/Cliente.java
@@ -1,5 +1,26 @@
 package br.com.fiap.beans;
 
-public class Cliente {
+public class Cliente extends Pessoa{
+	String razaoSocial;
+	String cnpj;
+	String inscricaoEstadural;
+	String email;
+	String password;
+	
+	
+	public Cliente() {}
 
+
+	public Cliente(String nome, String razaoSocial, String cnpj, String inscricaoEstadural, String email, String password) {
+		super(nome);
+		this.razaoSocial = razaoSocial;
+		this.cnpj = cnpj;
+		this.inscricaoEstadural = inscricaoEstadural;
+		this.email = email;
+		this.password = password;
+	}
+	
+	
+
+	
 }
