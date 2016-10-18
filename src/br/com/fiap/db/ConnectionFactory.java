da4d52679c0918cc6e3611993140355f7566a9e6
darth.cabs <dav.cardoso@gmail.com>
2016-10-18 09:22:26 -0200

    TarefaDAO
    Initial commit

--- src/br/com/fiap/db/ConnectionFactory.java
+++ src/br/com/fiap/db/ConnectionFactory.java
@@ -4,12 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionFactory {
+	/*
+	jdbc:oracle:thin:@192.168.60.15:1521:ORCL
+	jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL
+	OPS$RM76207
+	040696
+	*/		
+	
+	
 	public Connection getConnection() throws Exception {
 		String db = "oracle";
-		String url = "localhost:1521";
-		String instance = "XE";
-		String user = "david";
-		String passwd = "davifjc";
+		String url = "@192.168.60.15";
+		String instance = "ORCL";
+		String user = "OPS$RM76860";
+		String passwd = "";
 		
 		if (db.equals("oracle")) {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
