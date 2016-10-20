dd4c5586edd1c1f39b31caa4fa1ea5df4d08cf53
rm76539 <rm76539@PA05MICRO03.fiap.com.br>
2016-10-20 09:42:42 -0200

    Last Update

--- NAC/src/br/com/fiap/beans/Processo.java
+++ NAC/src/br/com/fiap/beans/Processo.java
@@ -6,6 +6,7 @@ public class Processo {
 	long numero;
 	Cliente cliente;
 	Causa especialidade;
+	Advogado advogado;
 	List<Honorario> honorario;
 	Forum forum;
 	List<Despesa> despesa;
@@ -17,10 +18,19 @@ public class Processo {
 	int situacao;
 	String observacao;
 	Taxa taxa;
+	
+	/**
+	 * 
+	 */
+	public Processo() {
+		// TODO Auto-generated constructor stub
+	}
+
 	/**
 	 * @param numero
 	 * @param cliente
 	 * @param especialidade
+	 * @param advogado
 	 * @param honorario
 	 * @param forum
 	 * @param despesa
@@ -33,12 +43,13 @@ public class Processo {
 	 * @param observacao
 	 * @param taxa
 	 */
-	public Processo(long numero, Cliente cliente, Causa especialidade, List<Honorario> honorario, Forum forum,
-			List<Despesa> despesa, String descricao, String dataAbertura, String dataFechamento, String dataVencimento,
-			int resultado, int situacao, String observacao, Taxa taxa) {
+	public Processo(long numero, Cliente cliente, Causa especialidade, Advogado advogado, List<Honorario> honorario,
+			Forum forum, List<Despesa> despesa, String descricao, String dataAbertura, String dataFechamento,
+			String dataVencimento, int resultado, int situacao, String observacao, Taxa taxa) {
 		this.numero = numero;
 		this.cliente = cliente;
 		this.especialidade = especialidade;
+		this.advogado = advogado;
 		this.honorario = honorario;
 		this.forum = forum;
 		this.despesa = despesa;
@@ -51,180 +62,223 @@ public class Processo {
 		this.observacao = observacao;
 		this.taxa = taxa;
 	}
-	
+
 	public Processo(long numero, String descricao, Forum forum){
 		this.numero = numero;
 		this.descricao = descricao;
 		this.forum = forum;
 	}
+	
 	/**
 	 * @return the numero
 	 */
 	public long getNumero() {
 		return numero;
 	}
+
 	/**
 	 * @param numero the numero to set
 	 */
 	public void setNumero(long numero) {
 		this.numero = numero;
 	}
+
 	/**
 	 * @return the cliente
 	 */
 	public Cliente getCliente() {
 		return cliente;
 	}
+
 	/**
 	 * @param cliente the cliente to set
 	 */
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+
 	/**
 	 * @return the especialidade
 	 */
 	public Causa getEspecialidade() {
 		return especialidade;
 	}
+
 	/**
 	 * @param especialidade the especialidade to set
 	 */
 	public void setEspecialidade(Causa especialidade) {
 		this.especialidade = especialidade;
 	}
+
+	/**
+	 * @return the advogado
+	 */
+	public Advogado getAdvogado() {
+		return advogado;
+	}
+
+	/**
+	 * @param advogado the advogado to set
+	 */
+	public void setAdvogado(Advogado advogado) {
+		this.advogado = advogado;
+	}
+
 	/**
 	 * @return the honorario
 	 */
 	public List<Honorario> getHonorario() {
 		return honorario;
 	}
+
 	/**
 	 * @param honorario the honorario to set
 	 */
 	public void setHonorario(List<Honorario> honorario) {
 		this.honorario = honorario;
 	}
+
 	/**
 	 * @return the forum
 	 */
 	public Forum getForum() {
 		return forum;
 	}
+
 	/**
 	 * @param forum the forum to set
 	 */
 	public void setForum(Forum forum) {
 		this.forum = forum;
 	}
+
 	/**
 	 * @return the despesa
 	 */
 	public List<Despesa> getDespesa() {
 		return despesa;
 	}
+
 	/**
 	 * @param despesa the despesa to set
 	 */
 	public void setDespesa(List<Despesa> despesa) {
 		this.despesa = despesa;
 	}
+
 	/**
 	 * @return the descricao
 	 */
 	public String getDescricao() {
 		return descricao;
 	}
+
 	/**
 	 * @param descricao the descricao to set
 	 */
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	/**
 	 * @return the dataAbertura
 	 */
 	public String getDataAbertura() {
 		return dataAbertura;
 	}
+
 	/**
 	 * @param dataAbertura the dataAbertura to set
 	 */
 	public void setDataAbertura(String dataAbertura) {
 		this.dataAbertura = dataAbertura;
 	}
+
 	/**
 	 * @return the dataFechamento
 	 */
 	public String getDataFechamento() {
 		return dataFechamento;
 	}
+
 	/**
 	 * @param dataFechamento the dataFechamento to set
 	 */
 	public void setDataFechamento(String dataFechamento) {
 		this.dataFechamento = dataFechamento;
 	}
+
 	/**
 	 * @return the dataVencimento
 	 */
 	public String getDataVencimento() {
 		return dataVencimento;
 	}
+
 	/**
 	 * @param dataVencimento the dataVencimento to set
 	 */
 	public void setDataVencimento(String dataVencimento) {
 		this.dataVencimento = dataVencimento;
 	}
+
 	/**
 	 * @return the resultado
 	 */
 	public int getResultado() {
 		return resultado;
 	}
+
 	/**
 	 * @param resultado the resultado to set
 	 */
 	public void setResultado(int resultado) {
 		this.resultado = resultado;
 	}
+
 	/**
 	 * @return the situacao
 	 */
 	public int getSituacao() {
 		return situacao;
 	}
+
 	/**
 	 * @param situacao the situacao to set
 	 */
 	public void setSituacao(int situacao) {
 		this.situacao = situacao;
 	}
+
 	/**
 	 * @return the observacao
 	 */
 	public String getObservacao() {
 		return observacao;
 	}
+
 	/**
 	 * @param observacao the observacao to set
 	 */
 	public void setObservacao(String observacao) {
 		this.observacao = observacao;
 	}
+
 	/**
 	 * @return the taxa
 	 */
 	public Taxa getTaxa() {
 		return taxa;
 	}
+
 	/**
 	 * @param taxa the taxa to set
 	 */
 	public void setTaxa(Taxa taxa) {
 		this.taxa = taxa;
 	}
+	
 	
 	
 }
